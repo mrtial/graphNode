51caad543683e43e80feb386ba64e62c72513ff0
mrtial <mrtial@gmail.com>
2016-09-17 13:52:29 -0700

    move d3 function to service

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -1,45 +1,44 @@
-var temp=0;
-
 (function(){
 	angular.module("App")
 	.controller("mainController", mainController)
 
-
-	function mainController($http, $api, $rootScope){
+	function mainController($http, $api, $rootScope, $d3){
 		var vm = this;
 		vm.menuShow = false;
 		vm.editType="json";
 
-		vm.modalOpen = function(){
-			vm.open = true;
-		}
-
-		vm.modalClose = function(e){
-			if(e && e.target.id==="modal_window"){
-				vm.open = false;
-				vm.menuShow = false;
-			}
-		}
-
-
-
-
+		// Toolbar Function
+		// =================================================
     // GET DATA
   	vm.getData = function(id){
   		$api.getData(id)
   		.then(function successCallback(response) {
   			vm.data = response.data;
 	      // RE-STRUCTURE DATA FOR D3
-	      vm.treeData = build(vm.nodeID, vm.data);
+	      vm.treeData = $d3.build(vm.nodeID, vm.data);
 	      console.log(vm.data);
 	      // BUILD D3
-	      generateD3(vm.treeData);
+	      $d3.generateD3(vm.treeData);
 		  }, function errorCallback(err) {
 		    console.log(err)
 		  });
   	}
 
+  	// Modal Function : Open / Close
+  	// =================================================
+  	vm.modalOpen = function(){
+  		vm.open = true;
+  	}
+
+  	vm.modalClose = function(e){
+  		if(e && e.target.id==="modal_window"){
+  			vm.open = false;
+  			vm.menuShow = false;
+  		}
+  	}
+
 	  // TOGGLE MENU
+	  // =================================================
 	  vm.toggleMenu = function(e){
 	  	// console.log(obj);
 	  	// show menu at mouse click position
@@ -50,12 +49,22 @@ var temp=0;
 	  	if(e.target.tagName==="rect"){
 	  		vm.menuShow = true;
 	  		var obj = e.target.__data__;
+	  		console.log(obj)
 
 	  		// find current node in vm.data
 	  		// bind current node json to $rootScope
+	  		if (obj.button){
+	  			var jsonData = findNode(obj.parent.id, vm.data);
+	  			vm.currentID = obj.parent.id;
 
-	  		let data = findNode(obj.id, vm.data);
-	  		vm.json = prettyPrint(JSON.stringify(data));
+	  		}else{
+	  			var jsonData = findNode(obj.id, vm.data);
+	  			vm.currentID = obj.id;
+	  		}
+
+	  		vm.json = prettyPrint(JSON.stringify(jsonData));
+
+
 
 	  		vm.currentNode = obj;
 
@@ -86,21 +95,46 @@ var temp=0;
 	  // MENU FUNCTION
 	  // ===============================================
 	  // ADD NODE
+	  // 1. get id from db
+	  // 2. post new node to db
+	  // 3. render d3
 	  vm.addNode = function(node_name,data){
 	  	add_node(node_name,data);
 	  	vm.menuShow = false;
-	  	generateD3(vm.treeData);
-
+	  	$d3.generateD3(vm.treeData);
 	  }
 
 	  // DELETE NODE
+	  // 1. delete id from db
+	  // 2. render d3
 	  vm.deleteNode = function(node_name,data){
 	  	delete_node(node_name, data);
 	  	vm.menuShow = false;
-	  	generateD3(vm.treeData);
+	  	$d3.generateD3(vm.treeData);
 	  }
 
-	  // EDIT JSON
+	  // EDIT JSON / EDIT TITLE / EDIT MSG
+	  // 1. put data to db
+	  // 2. render d3
+
+
+	  // updateDB
+	  vm.updateDB = function(){
+	  	$api.updateData(vm.currentID, vm.json)
+	  	.then(function success(response){
+	  		debugger
+	  		console.log("success: " +response)
+	  	}, function fail(response){
+	  		console.log("falied: "+response)
+	  	})
+	  	// .then(function(){
+	  	// 	vm.getData(vm.nodeID);
+	  	// }, function(err){console.log(err)}).then(function(){
+	  	// 	vm.modalClose();
+	  	// })
+	  	
+	  }
+
 
 	} // mainController
 
@@ -121,36 +155,6 @@ var temp=0;
 
 	// D3 FUNCTION
 	// =================================================
-	// TODO: Move to service
-	function build(node_name, data) { 
-	  // Remove previous svg if exist
-	  removeNode()
-
-	  var node = data[data.map(function(e) {return e._id}).indexOf(node_name)];
-	  
-	  var tree =    { 
-	    "id" : node._id,
-	    "text" : node.message_text,
-	    "button" : false,
-	    "hidden" : node.type === "hidden",
-	    "payload_type" : node.payload_type,
-	    "children" : []
-	  };
-	  node.buttons.forEach(function(b,i){
-
-	    tree.children.push({
-	      "id" : node._id + "_button" + i,
-	      "text" : b.title,
-	      "button" : true,
-	      "hidden" : b.type === "hidden",
-	      "payload_type" : b.type,
-	      "children" : [build(b.next_node_id, data)]
-	    });
-	  });
-	  return tree;
-	}
-
-
 	function delete_node(node_name,treeData){
 	  removeNode()
 	  if('children' in treeData)
@@ -166,6 +170,8 @@ var temp=0;
 	  return treeData;
 	}
 
+	var temp=0;
+	
 	function add_node(node_name,treeData){
 	  removeNode()
 
@@ -213,168 +219,6 @@ var temp=0;
 	      t = add_node(node_name,t);
 	   	});}
 	  return treeData;
-	}
-
-
-	function generateD3(treeData){
-	  var margin = {top: 40, right: 120, bottom: 20, left: 120},
-	      width = 1200 - margin.right - margin.left,
-	      height = 1200 - margin.top - margin.bottom;
-	  var boxHeight = 50, boxWidth = 400;
-	  var i = 0;
-
-	  var tree = d3.layout.tree()
-	                      .size([width,height]);
-
-	  var diagonal = d3.svg.diagonal()
-	    .projection(function(d) { return [d.x, d.y]; });
-
-	  var svg = d3.select("#d3_graph").append("svg")
-	    .attr("width", width + margin.right + margin.left)
-	    .attr("height", height + margin.top + margin.bottom)
-	    .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	  root = treeData;
-	    
-	  update(root);
-
-	  function update(source) {
-
-	    // Compute the new tree layout.
-	    var nodes = tree.nodes(root).reverse(),
-	        links = tree.links(nodes);
-
-	    // Normalize for fixed-depth.
-	    nodes.forEach(function(d) { d.y = d.depth * 100; });
-
-	    // Declare the nodes…
-	    var node = svg.selectAll("g.node")
-	      .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-	    // Enter the nodes.
-	    var nodeEnter = node.enter().append("g")
-	      .attr("class", "node")
-	      .attr("transform", function(d) { 
-	        return "translate(" + d.x + "," + d.y + ")"; })
-	      
-	      // EventListener on CLICK 
-	      // .on("click",function(d){
-	      // 	var e = d3.event;
-	      // 	vm.toggleMenu(e);
-
-	        // if(d.button){
-	        //   delete_node(d.id,treeData);
-	        // }else{
-	        //   delete_node(d.parent.id,treeData)
-	        // }
-	        // generateD3(treeData);
-	        // });
-
-	    //get width for each depth
-	    var maxDepth = Math.max.apply(Math,d3.selectAll("g.node").data().map(function(o){return o.depth;}));
-	    
-	    var LevelBoxWidth = new Array(maxDepth).fill(0);
-		
-	    LevelBoxWidth.forEach(function(d,i){
-	      var Objs = d3.selectAll("g.node").data().filter(function(d1){return d1.depth==(i+1)});
-	      var Objs_x =[];
-	      Objs.forEach(function(d1){Objs_x.push(d1.x)});
-	      Objs_x.sort(function(a, b){return b-a});
-	      maxGap=boxWidth;
-		      for(j=0;j<(Objs_x.length-1);j++){
-		      	maxGap = Math.min(maxGap,(Objs_x[j]-Objs_x[j+1]-5));
-		      }
-	 	  LevelBoxWidth[i] = maxGap;
-	     });
-
-	    nodeEnter.append("rect")
-	      .attr("width",function(d){
-	        if(d.depth===0){
-	          return boxWidth;
-	        }else{
-	          return LevelBoxWidth[d.depth-1];
-	        }
-	       })
-	      .attr("x",function(d){return -1*d3.select(this).attr("width")/2;})
-	      .attr("height", boxHeight)
-	      .attr("y", -1*boxHeight/2)
-	      .attr("stroke", function(d){
-	        if(d.payload_type === "bubble"){
-	          return "#A9CCE3";
-	        }else if (d.hidden === true){
-	          return "#82E0AA";
-	        }else{
-	          return "#D98880";
-	        }
-	      })
-	      .attr("fill",function(d){
-	      	if(d.button===true)
-	      	{
-	      		return d3.select(this).attr("stroke");
-	      	}else{
-	      		return "#F2F2F2";
-	      	}
-
-	      });
-
-
-	    nodeEnter.append("text")
-	      .attr("dy", ".35em")
-	      .attr("text-anchor", "middle")
-	      .html(function(d) {  return d.text; })
-	      .style("fill-opacity", 1)
-	      .call(wrap);
-
-	    // Dynamically adjust the height and width of message box
-	    d3.selectAll("rect").attr("height",function(d){return boxHeight + 10* (this.parentNode.lineNumber-1) ;})
-
-	    // Declare the links…
-	    var link = svg.selectAll("path.link")
-	      .data(links, function(d) { return d.target.id; });
-
-	    // Enter the links.
-	    link.enter().insert("path", "g")
-	      .attr("class", "link")
-	      .attr("d", diagonal);
-	    
-	    function wrap(text) {
-	      text.each(function(t) {
-	        var width = d3.select(this.parentNode.childNodes[0]).attr("width")-20;
-	        var text = d3.select(this),
-	            words = text.text().split(/\s+/).reverse(),
-	            word,
-	            line = [],
-	            lineNumber = 1,
-	            lineHeight = 1.0, // ems
-	            y = text.attr("y"),
-	            dy = parseFloat(text.attr("dy")),
-	            tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-	            this.parentNode.lineNumber = 1;
-	        while (word = words.pop()) {
-	          line.push(word);
-	          tspan.text(line.join(" "));
-	          if (tspan.node().getComputedTextLength() > width) {
-
-	            line.pop();
-	            tspan.text(line.join(" "));
-	            line = [word];
-	            tspan = text.append("tspan").attr("x", 0).attr("y", 0).attr("dy", lineNumber * lineHeight + dy + "em").text(word);
-	            lineNumber = lineNumber +1;
-	          }
-	          //d3.select(this.parentNode).attr("lineNumber",lineNumber);
-	          this.parentNode.lineNumber = lineNumber;
-	        }
-	      });
-	    } // wrap 
-	  } // update
-	}
-
-	function removeNode(){
-	  var node = document.getElementById('d3_graph');
-	  while (node && node.firstChild) {
-	      node.removeChild(node.firstChild);
-	  }
 	}
 
 })()
--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -2,8 +2,10 @@
 
 	angular.module("App")
 		.service("$api", api)
+		.service("$d3", d3graph)
 
-		// API FUNCTION
+		// API FUNCTION 
+		// ======================================================
 		function api($http){
 			var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlzaGEiLCJpYXQiOjE0Njc3ODc1MjUsImV4cCI6MTQ2Nzg3MzkyNX0.Hljv7vcSWX9G2N11-XJTuZvnZP4PiGZMJJYm2tIGwD0";
 
@@ -26,7 +28,7 @@
 				     'x-access-token': token,
 				     'Cache-Control': 'no-cache',
 				  },
-				  data:{data}
+				  data:data
 				})
 			};
 
@@ -53,7 +55,7 @@
 					   'x-access-token': token,
 					   'Cache-Control': 'no-cache',
 					},
-					dtat:{data}
+					data:JSON.parse(data)
 				})
 			};
 
@@ -80,8 +82,191 @@
 					url: `http://remissionaire-staging.herokuapp.com/api/v1/repopulate-nodes`,
 				})
 			}
+		}
+
+		// D3 FUNCTION
+		// ======================================================
+		function d3graph(){
+			this.build = function(node_name, data){
+			  var self = this;
+			  // Remove previous svg if exist
+			  removeNode()
+
+			  var node = data[data.map(function(e) {return e._id}).indexOf(node_name)];
+			  
+			  var tree =    { 
+			    "id" : node._id,
+			    "text" : node.message_text,
+			    "button" : false,
+			    "hidden" : node.type === "hidden",
+			    "payload_type" : node.payload_type,
+			    "children" : []
+			  };
+
+			  node.buttons.forEach(function(b,i){
+
+			    tree.children.push({
+			      "id" : node._id + "_button" + i,
+			      "text" : b.title,
+			      "button" : true,
+			      "hidden" : b.type === "hidden",
+			      "payload_type" : b.type,
+			      "children" : [self.build(b.next_node_id, data)]
+			    });
+			  });
+			  return tree;
+			}
 
 
+			this.generateD3= function(treeData){
+			  var margin = {top: 40, right: 120, bottom: 20, left: 120},
+			      width = 1200 - margin.right - margin.left,
+			      height = 1200 - margin.top - margin.bottom;
+			  var boxHeight = 50, boxWidth = 400;
+			  var i = 0;
+
+			  var tree = d3.layout.tree()
+			                      .size([width,height]);
+
+			  var diagonal = d3.svg.diagonal()
+			    .projection(function(d) { return [d.x, d.y]; });
+
+			  var svg = d3.select("#d3_graph").append("svg")
+			    .attr("width", width + margin.right + margin.left)
+			    .attr("height", height + margin.top + margin.bottom)
+			    .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			  root = treeData;
+			    
+			  update(root);
+
+			  function update(source) {
+
+			    // Compute the new tree layout.
+			    var nodes = tree.nodes(root).reverse(),
+			        links = tree.links(nodes);
+
+			    // Normalize for fixed-depth.
+			    nodes.forEach(function(d) { d.y = d.depth * 100; });
+
+			    // Declare the nodes…
+			    var node = svg.selectAll("g.node")
+			      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+			    // Enter the nodes.
+			    var nodeEnter = node.enter().append("g")
+			      .attr("class", "node")
+			      .attr("transform", function(d) { 
+			        return "translate(" + d.x + "," + d.y + ")"; })
+
+			    //get width for each depth
+			    var maxDepth = Math.max.apply(Math,d3.selectAll("g.node").data().map(function(o){return o.depth;}));
+			    
+			    var LevelBoxWidth = new Array(maxDepth).fill(0);
+				
+			    LevelBoxWidth.forEach(function(d,i){
+			      var Objs = d3.selectAll("g.node").data().filter(function(d1){return d1.depth==(i+1)});
+			      var Objs_x =[];
+			      Objs.forEach(function(d1){Objs_x.push(d1.x)});
+			      Objs_x.sort(function(a, b){return b-a});
+			      maxGap=boxWidth;
+				      for(j=0;j<(Objs_x.length-1);j++){
+				      	maxGap = Math.min(maxGap,(Objs_x[j]-Objs_x[j+1]-5));
+				      }
+			 	  LevelBoxWidth[i] = maxGap;
+			     });
+
+			    nodeEnter.append("rect")
+			      .attr("width",function(d){
+			        if(d.depth===0){
+			          return boxWidth;
+			        }else{
+			          return LevelBoxWidth[d.depth-1];
+			        }
+			       })
+			      .attr("x",function(d){return -1*d3.select(this).attr("width")/2;})
+			      .attr("height", boxHeight)
+			      .attr("y", -1*boxHeight/2)
+			      .attr("stroke", function(d){
+			        if(d.payload_type === "bubble"){
+			          return "#A9CCE3";
+			        }else if (d.hidden === true){
+			          return "#82E0AA";
+			        }else{
+			          return "#D98880";
+			        }
+			      })
+			      .attr("fill",function(d){
+			      	if(d.button===true)
+			      	{
+			      		return d3.select(this).attr("stroke");
+			      	}else{
+			      		return "#F2F2F2";
+			      	}
+
+			      });
+
+
+			    nodeEnter.append("text")
+			      .attr("dy", ".35em")
+			      .attr("text-anchor", "middle")
+			      .html(function(d) {  return d.text; })
+			      .style("fill-opacity", 1)
+			      .call(wrap);
+
+			    // Dynamically adjust the height and width of message box
+			    d3.selectAll("rect").attr("height",function(d){return boxHeight + 10* (this.parentNode.lineNumber-1) ;})
+
+			    // Declare the links…
+			    var link = svg.selectAll("path.link")
+			      .data(links, function(d) { return d.target.id; });
+
+			    // Enter the links.
+			    link.enter().insert("path", "g")
+			      .attr("class", "link")
+			      .attr("d", diagonal);
+			    
+			    function wrap(text) {
+			      text.each(function(t) {
+			        var width = d3.select(this.parentNode.childNodes[0]).attr("width")-20;
+			        var text = d3.select(this),
+			            words = text.text().split(/\s+/).reverse(),
+			            word,
+			            line = [],
+			            lineNumber = 1,
+			            lineHeight = 1.0, // ems
+			            y = text.attr("y"),
+			            dy = parseFloat(text.attr("dy")),
+			            tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+			            this.parentNode.lineNumber = 1;
+			        while (word = words.pop()) {
+			          line.push(word);
+			          tspan.text(line.join(" "));
+			          if (tspan.node().getComputedTextLength() > width) {
+
+			            line.pop();
+			            tspan.text(line.join(" "));
+			            line = [word];
+			            tspan = text.append("tspan").attr("x", 0).attr("y", 0).attr("dy", lineNumber * lineHeight + dy + "em").text(word);
+			            lineNumber = lineNumber +1;
+			          }
+			          //d3.select(this.parentNode).attr("lineNumber",lineNumber);
+			          this.parentNode.lineNumber = lineNumber;
+			        }
+			      });
+			    } // wrap 
+			  } // update
+			}
+
+		}
+
+		// Helper Function
+		function removeNode(){
+		  var node = document.getElementById('d3_graph');
+		  while (node && node.firstChild) {
+		      node.removeChild(node.firstChild);
+		  }
 		}
 
 })()
